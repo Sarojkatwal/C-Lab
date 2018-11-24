01ff9748fcafa5aeb71a23fa539ca0b89784e15f
Sarojkatwal <katwalsaroj11@gmail.com>
2018-11-24 08:18:26 +0545

    Added

---
+++ acd.py
@@ -0,0 +1,3 @@
+import pandas as p
+import numpy as np
+df=p.read_excel("new.xlsx","Weather",parse_date=['day'])
---
+++ example.py
@@ -0,0 +1,5 @@
+import random as r
+import numpy as np
+a=np.arange(1,31)
+b="1 2 3 4 5 6 7 8 9"
+c=[' ']*10
